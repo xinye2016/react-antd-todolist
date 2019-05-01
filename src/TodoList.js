af59df5a44fce57f8c3b055753e21c44e3d129fd
新页 <xinye@xinyedeMacBook-Pro.local>
2019-05-01 20:11:18 +0800

    项目提交

---
+++ src/TodoList.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import { Layout } from 'antd';
+import Form from './components/Form';
+import DataList from './components/DataList';
+import Footer from './components/Footer';
+import datas from './data'
+import './todolist.less'
+
+const { Header, Content} = Layout;
+
+class TodoList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      list: []
+    }
+  }
+  componentDidMount () {
+    this.setState({
+      list: datas
+    })
+  }
+
+  deleteItem (id) {
+    let deleteIndex = datas.findIndex(item => {
+      return item.id === id
+    })
+    datas.splice(deleteIndex, 1)
+    this.setState({
+      list: datas
+    })
+  }
+
+  changeItem (id) {
+    let changeIndex = datas.findIndex(item => {
+      return item.id === id
+    })
+    datas[changeIndex].isComplete = !datas[changeIndex].isComplete
+    this.setState({
+      list: datas
+    })
+  }
+
+  handleSearchItem(value) {
+    let newList = datas.filter(item => {
+      return item.content.indexOf(value) !== -1
+    })
+    this.setState({
+      list: newList
+    })
+  }
+
+  addItem (item) {
+    datas.push(item)
+    this.setState({
+      list: datas
+    })
+  }
+
+  render () {
+    return (
+      <Layout className="todolist-layout">
+        <Header>
+        <h3 className="logo">TodoList</h3>
+        </Header>
+        <Content className="todolist-content">
+          <Form searchItem={value => this.handleSearchItem(value)}></Form>
+          <DataList list={this.state.list} deleteItem={id => this.deleteItem(id)} changeItem={id => this.changeItem(id)}></DataList>
+          <Footer addItem={item => this.addItem(item)}></Footer>
+        </Content>
+      </Layout>
+    )
+  }
+}
+
+export default TodoList;
